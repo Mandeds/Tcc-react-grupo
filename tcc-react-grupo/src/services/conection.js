5f6333f0253f73fe18dd159895e07e56a011badb
Calanguinho <ra45012145895@acaonsfatima.org.br>
2025-10-12 21:59:26 -0300

    Add .env config and update MySQL connection
    Introduced a .env file for environment variables and updated the MySQL connection in conection.js to use dotenv and process.env values. Switched from createConnection to createPool for improved connection management.

--- tcc-react-grupo/src/services/conection.js
+++ tcc-react-grupo/src/services/conection.js
@@ -1,9 +1,11 @@
 import mysql2 from "mysql2"
+import dotenv from 'dotenv'
+dotenv.config();
 
-const conection = await mysql2.createConnection({
-    host: `localhost`,
-    password: `1234`,
-    user:`root`,
+const conection = await mysql2.createPool({
+    host: process.env.db_host,
+    password: process.env.db_password,
+    user:  process.env.db.db_user,
     database: 'tcc_frei'
 })
 
