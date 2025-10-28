9110ecb086030ba8e1aba833c42cca82f08fca75
Thiago-doFrei <ra48607520822@acaonsfatima.org.br>
2025-10-28 09:45:03 -0300

    Inicio do front end da pagina de mensagens
    Added new images for user profile and message background. Updated navegarPost component to include user profile section, messaging area, and navigation options. Introduced new SCSS styles for layout and appearance of navigation, profile, and message sections.

--- frontend/src/pages/nav2/navegarPost/naveg.jsx
+++ frontend/src/pages/nav2/navegarPost/naveg.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import "./naveg.scss"
+import "./Naveg.scss"
+import user from "/images/user.png"
 import CabecalhoNav from "../../../component/cabecalhonav/cabecalhonav";
 
 export default function Naveg (){
@@ -7,12 +8,28 @@ export default function Naveg (){
     return(
         <>
          <CabecalhoNav/>
-         <div className="Lado1"></div>
-            <div className="Main_naveg">
-
+            <div className="cabecalho_nav">
+                <h1>Hello word</h1>
             </div>
-            <div className="Lado2">
-                
+
+            <div className="container_navegacao"> 
+
+            <div className="perfil_esquerda">
+            <h1>Hello word</h1>
+            </div>
+
+
+            <div className="mensagens">
+            <h1>Hello word</h1>
+            </div>
+
+            <div className="perfil_direita">
+                <img src={user} alt="usuario" id="img_usuario" width={80}/>
+                <h2 className="h2_mensagens">Inicio</h2>
+                <h2 className="h2_mensagens">Pesquisa</h2>
+                <h2 className="h2_mensagens">Notificações</h2>
+            </div>
+
             </div>
         </>
     )
