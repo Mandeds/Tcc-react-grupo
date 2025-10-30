e97c4cf4efdd6acea91d1dbeb444dca1ef69a5df
Thiago-doFrei <ra48607520822@acaonsfatima.org.br>
2025-10-30 09:11:29 -0300

    Update navigation UI to use buttons and new logo
    Replaced text options and headings with styled buttons for navigation actions. Updated the menu image to a new logo and adjusted SCSS for improved layout and button positioning. Removed unused imports and cleaned up unused elements.

--- frontend/src/pages/nav2/navegarPost/naveg.jsx
+++ frontend/src/pages/nav2/navegarPost/naveg.jsx
@@ -1,8 +1,7 @@
-import { Link } from "react-router-dom";
 import "./Naveg.scss"
 import user from "/images/user.png"
-import gato from "/images/imagem_gato.png"
 import menu from "/images/menuHamburguer.png"
+import logo from "/logo_tcc.png 1.png"
 import CabecalhoNav from "../../../component/cabecalhonav/cabecalhonav";
 
 export default function Naveg (){
@@ -11,27 +10,24 @@ export default function Naveg (){
         <>
          <CabecalhoNav/>
             <div className="cabecalho_nav">
-                <img src={menu} alt="menu" id="menu_hamburguer" width={55}/>
+                <img src={logo} alt="menu" id="menu_hamburguer" width={110}/>
 
-                <p className="opcao">Opção1</p>
-                <p className="opcao">Opção2</p>
+                <button id="opcao1">Opção1</button>
+                <button id="opcao2">Opção2</button>
                 <img src={user} alt="usuario" id="img_usuario" width={100}/>
             </div>
 
             <div className="container_navegacao"> 
 
-            <div className="perfil_esquerda">
-            <h1>Hello word</h1>
-            </div>
 
 
             <div className="mensagens">
             </div>
 
             <div className="perfil_direita">
-                <h2 className="h2_mensagens">Inicio</h2>
-                <h2 className="h2_mensagens">Pesquisa</h2>
-                <h2 className="h2_mensagens">Notificações</h2>
+                <button className="h2_mensagens">Inicio</button>
+                <button className="h2_mensagens">Pesquisa</button>
+                <button className="h2_mensagens">Notificações</button>
             </div>
 
             </div>
